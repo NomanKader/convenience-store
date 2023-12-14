613bbec393d24a3da4669d89e6ecbf0564937d48
NomanKader <nomankader786@gmail.com>
2023-12-14 15:30:57 +0630

    Added simple build

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"H0r1BffEzwEohwar6JucyzkmkMGqAI1qgc+GSKBdR/M=\"}"
---
+++ .next/static/NYGcb0hDsa_GbfP0nBU5A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[{has:void 0,source:"/api/:path*",destination:"/api-handler"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e87e5963ec1b8011.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
